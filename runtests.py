62ec48067978accaae1ef85940ff4585b2570603
M1ha <m1hacka@mail.ru>
2018-09-08 12:52:14 +0500

    1) Fixed tests 2) Added django 2.1 to travis

--- runtests.py
+++ runtests.py
@@ -15,7 +15,7 @@ from django.test.utils import get_runner
 if __name__ == "__main__":
     print('Django: ', django.VERSION)
     print('Python: ', sys.version)
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
